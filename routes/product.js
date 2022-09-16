a10a35d7ffdcf9b2f993df07e59a4fa6b0462e88
YohanNayanajith <yohannayanajith40@gmail.com>
2022-09-17 01:44:50 +0530

    set authentication

--- routes/product.js
+++ routes/product.js
@@ -8,17 +8,6 @@ const {
 const router = require("express").Router();
 
 //CREATE
-
-// router.post("/", verifyTokenAndAdmin, async (req, res) => {
-//   const newProduct = new Product(req.body);
-
-//   try {
-//     const savedProduct = await newProduct.save();
-//     res.status(200).json(savedProduct);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 router.post("/", verifyTokenAndAuthorization, async (req, res) => {
   const newProduct = new Product(req.body);
 
@@ -31,20 +20,6 @@ router.post("/", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 //UPDATE
-// router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     const updatedProduct = await Product.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: req.body,
-//       },
-//       { new: true }
-//     );
-//     res.status(200).json(updatedProduct);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     const updatedProduct = await Product.findByIdAndUpdate(
@@ -61,14 +36,6 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 //DELETE
-// router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     await Product.findByIdAndDelete(req.params.id);
-//     res.status(200).json("Product has been deleted...");
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     await Product.findByIdAndDelete(req.params.id);
